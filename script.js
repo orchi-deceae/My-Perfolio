f12f671496cf649648d57dd8611ab2a38ee9cc2b
oksherlock <oksherlock9661@gmail.com>
2025-01-23 19:43:28 +0100

    1.5

---
+++ script.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_1agty0e","template_tt7nbeg",parms).then(alert("Email Sent!!!"))
+}
