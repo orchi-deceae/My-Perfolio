8131c450ae0fc9fd1a259026011fe3429a32fe53
oksherlock <oksherlock9661@gmail.com>
2025-01-30 15:12:15 +0100

    2.1 Skip btn

--- script2.js
+++ script2.js
@@ -9,3 +9,33 @@ function sendMail(){
     emailjs.send("service_1agty0e","template_tt7nbeg",parms).then(alert("Email Sent!!! -by Elvis"))
 }
 
+
+
+function fastForwardAnimations() {
+    document.querySelectorAll("*").forEach(
+        el => {
+            let computedstyle = window.getComputedStyle(el);
+            let animationDuration = computedstyle.getPropertyValue("animation-duration");
+            let animationDelay = computedstyle.getPropertyValue("animation-delay");
+
+            let duration = parseFloat(animationDuration) ||0;
+            let delay = parseFloat(animationDelay) ||0;
+
+            if (duration > 0) {
+                el.style.animationDelay = `-${duration + delay}s`;
+                el.style.animationIterationCount = "1";
+            }
+        }
+    );
+}
+
+document.getElementById("skipAnimation").addEventListener("click", fastForwardAnimations);
+
+document.addEventListener(
+    "keydown", function(event){
+        if (event.key.toLowerCase() === "c") {
+            fastForwardAnimations();
+        }
+    }
+);
+
